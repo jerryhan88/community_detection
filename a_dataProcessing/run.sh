3da9317567be6aad3ed1d8fa72dd8a4a972f3b64
ckhan <chungkyun.han@gmail.com>
2017-06-03 15:45:39 +0800

    temp

--- a_dataProcessing/run.sh
+++ a_dataProcessing/run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
 for i in 0{1..9} {10..12}; do
-    python -c "from a3_driverTrip import run; run('09$i')" &
+    python -c "from a2_dwellTimeNpriorPresence import process_month; process_month('09$i')" &
 done
+
+
+#python -c "from a3_driverTrip import run; run()" &
+
