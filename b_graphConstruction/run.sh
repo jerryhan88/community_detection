0ffabd6f8b07ee6fa776d37f054c1ef2a45dfcc8
ckhan <chungkyun.han@gmail.com>
2017-06-04 16:57:06 +0800

    temp

--- b_graphConstruction/run.sh
+++ b_graphConstruction/run.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-for i in {0..10}; do
-    python -c "from b0_individualCounting import run; run($i)" &
-done
+#for i in {0..10}; do
+#    python -c "from b0_individualCounting import run; run($i)" &
+#done
 
 #for i in {0..10}; do
 #    python -c "from b1_individualRelation import run; run($i)" &
 #done
+
+for i in {0..10}; do
+    python -c "from b1_individualRelationF import run; run($i)" &
+done
